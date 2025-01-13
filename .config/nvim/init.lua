c5ff01df41db3aa545abb34b9a4dcbd5f9f7c0cd
RandFloat <random.randfloat@proton.me>
2025-01-12 22:04:57 -0700

    Updated

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,6 +5,47 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = false
 
+require("nvim-treesitter.configs").setup {
+	highlight = {
+		enable = true,
+	},
+}
+
+local cmp = require("cmp")
+local lspkind = require("lspkind")
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			vim.snippet.expand(args.body)
+		end,
+	},
+	window = {
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+	}, {
+		-- { name = "buffer" },
+	}),
+	formatting = {
+		format = lspkind.cmp_format({
+			mode = "symbol",
+		}),
+	},
+})
+
+local lsp = require("lspconfig")
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+lsp.clangd.setup({
+	capabilities = capabilities
+})
+
 -- Plugins
 return require("packer").startup(function(use)
 
@@ -19,5 +60,12 @@ return require("packer").startup(function(use)
 		}
 	}
 
-end)
+	use "dense-analysis/ale"
+	use "nvim-treesitter/nvim-treesitter"
 
+	use "neovim/nvim-lspconfig"
+	use "hrsh7th/cmp-nvim-lsp"
+	use "hrsh7th/nvim-cmp"
+	use "onsails/lspkind.nvim"
+
+end)
