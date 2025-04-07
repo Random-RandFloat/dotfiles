855839d4882e51c75aad6f5f3f494b886b0aca14
RandFloat <random.randfloat@proton.me>
2025-04-07 08:03:43 -0600

    use pcall for convenience

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -45,55 +45,57 @@ map("n", "<C-E>", "<Cmd>lua vim.diagnostic.open_float()<CR>", opts)
 
 vim.g.barbar_auto_setup = false -- Disable auto-setup
 
-require("barbar").setup{
-	exclude_name = {"zsh"},
-}
+pcall(function()
+	require("barbar").setup{
+		exclude_name = {"zsh"},
+	}
 
-require("nvim-treesitter.configs").setup {
-	highlight = {
-		enable = true,
-	},
-}
+	require("nvim-treesitter.configs").setup {
+		highlight = {
+			enable = true,
+		},
+	}
 
-local cmp = require("cmp")
-local lspkind = require("lspkind")
-cmp.setup({
-	snippet = {
-		expand = function(args)
-			vim.snippet.expand(args.body)
-		end,
-	},
-	window = {
-	},
-	mapping = cmp.mapping.preset.insert({
-		['<C-b>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-e>'] = cmp.mapping.abort(),
-		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-	}),
-	sources = cmp.config.sources({
-		{ name = "nvim_lsp" },
-	}, {
-		-- { name = "buffer" },
-	}),
-	formatting = {
-		format = lspkind.cmp_format({
-			mode = "symbol",
+	local cmp = require("cmp")
+	local lspkind = require("lspkind")
+	cmp.setup({
+		snippet = {
+			expand = function(args)
+				vim.snippet.expand(args.body)
+			end,
+		},
+		window = {
+		},
+		mapping = cmp.mapping.preset.insert({
+			['<C-b>'] = cmp.mapping.scroll_docs(-4),
+			['<C-f>'] = cmp.mapping.scroll_docs(4),
+			['<C-Space>'] = cmp.mapping.complete(),
+			['<C-e>'] = cmp.mapping.abort(),
+			['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 		}),
-	},
-})
+		sources = cmp.config.sources({
+			{ name = "nvim_lsp" },
+		}, {
+			-- { name = "buffer" },
+		}),
+		formatting = {
+			format = lspkind.cmp_format({
+				mode = "symbol",
+			}),
+		},
+	})
+
+	local lsp = require("lspconfig")
+	local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	lsp.rust_analyzer.setup({})
+	lsp.gdscript.setup({})
+	lsp.pyright.setup({})
+	lsp.clangd.setup({
+		capabilities = capabilities
+	})
+end)
 
 vim.g.python_recommended_style = 0
-
-local lsp = require("lspconfig")
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-lsp.rust_analyzer.setup({})
-lsp.gdscript.setup({})
-lsp.pyright.setup({})
-lsp.clangd.setup({
-	capabilities = capabilities
-})
 
 -- Plugins
 return require("packer").startup(function(use)
