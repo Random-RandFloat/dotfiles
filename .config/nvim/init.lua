ffde448a9d069e97304cb13dec773462bffb82c9
RandFloat <random.randfloat@proton.me>
2025-05-13 08:47:04 -0600

    Format on save for NViM and .clang-format file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -95,13 +95,28 @@ pcall(function()
 
 	local lsp = require("lspconfig")
 	local capabilities = require("cmp_nvim_lsp").default_capabilities()
-	lsp.rust_analyzer.setup({})
-	lsp.gdscript.setup({})
-	lsp.pyright.setup({})
-	lsp.clangd.setup({
+	lsp.rust_analyzer.setup{}
+	lsp.gdscript.setup{}
+	lsp.pyright.setup{}
+	lsp.clangd.setup{
 		capabilities = capabilities
-	})
+	}
 end)
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+		if client:supports_method("textDocument/formatting") then
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				buffer = args.buf,
+				callback = function()
+					vim.lsp.buf.format({bufnr = args.buf, id = client.id})
+				end,
+			})
+		end
+	end,
+})
 
 vim.g.python_recommended_style = 0
 
--- update.sh
+++ update.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 
 [[ -f .zshrc ]] && rm .zshrc
-cp /home/randfloat/.zshrc .
+cp ~/.zshrc .
 [[ -d .config/nvim ]] && rm -r .config/nvim
-cp -r /home/randfloat/.config/nvim ./.config/nvim
-
+cp -r ~/.config/nvim ./.config/nvim
+[[ -f .clang-format ]] && rm .clang-format
+cp ~/.clang-format .
